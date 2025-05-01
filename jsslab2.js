a17834ff1174a5c67b7acc6800af1b867ed428f4
Sebaheddin007 <sbahddinrustmli08@gmail.com>
2025-05-01 19:05:53 +0400

    Add files via upload

---
+++ jsslab2.js
@@ -0,0 +1,6 @@
+
+document.getElementById("cvBtn").addEventListener("click", function() {
+    const messageEl = document.getElementById("cvMessage");
+    messageEl.textContent = "iOS Developer, 3 il təcrübə, Paşa Bank-da çalışır. AZTU məzunu, Swift və SwiftUI üzrə ixtisaslaşıb.";
+    messageEl.style.display = "block";
+});
